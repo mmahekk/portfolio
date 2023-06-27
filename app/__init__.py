e8af436cff434d81048b504a01e9baf440243f29
tzjg7293 <sydney.tj.fang@gmail.com>
2023-06-26 19:50:41 -0700

    Added route to flask for hobbies page

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,7 @@ app = Flask(__name__)
 def index():
     #OG: return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
     return render_template('index.html', url=os.getenv("URL"))
+
+@app.route('/hobbies')
+def hobby():
+    return render_template('hobbies.html', url=os.getenv("URL"))
